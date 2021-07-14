c2fa85d7fac28c81f18d815b559e0d4afdd6d2be
Lois Aleghe <loisaleghe@cmail.carleton.ca>
2021-07-14 12:43:12 -0400

    Lint file using black

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template, send_from_directory, json, request
 from dotenv import load_dotenv
+
 # from . import db
 from werkzeug.security import check_password_hash, generate_password_hash
+
 # from app.db import get_db
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,14 +12,17 @@ from flask_migrate import Migrate
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -26,7 +31,7 @@ migrate = Migrate(app, db)
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
 
@@ -38,32 +43,32 @@ class UserModel(db.Model):
         return f"<User {self.username}>"
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html', title="MLH DRAGON BLOG", url=os.getenv("URL"))
+    return render_template("index.html", title="MLH DRAGON BLOG", url=os.getenv("URL"))
 
 
 @app.route("/home")
 def index():
-    return render_template('index.html', title="MLH DRAGON BLOG", url=os.getenv("URL"))
+    return render_template("index.html", title="MLH DRAGON BLOG", url=os.getenv("URL"))
 
 
 # so we don't have to always go through environment variables
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
 
 
-@app.route('/blogs')
+@app.route("/blogs")
 def blog_page():
     dir = os.path.realpath(os.path.dirname(__file__))
     json_dir = os.path.join(dir, "static/data", "blogs.json")
     data = json.load(open(json_dir))
-    return render_template('blogs.html', blogs=data['blogs'])
+    return render_template("blogs.html", blogs=data["blogs"])
 
 
-@app.route('/showblog')
+@app.route("/showblog")
 def showblog():
-    id = int(request.args.get('id'))
+    id = int(request.args.get("id"))
 
     dir = os.path.realpath(os.path.dirname(__file__))
     json_dir = os.path.join(dir, "static/data", "blogs.json")
@@ -76,33 +81,33 @@ def showblog():
     # return render_template('showblog.html', blog=blog)
 
 
-@app.route('/projects')
+@app.route("/projects")
 def project_page():
-    return render_template('project.html')
+    return render_template("project.html")
 
 
-@app.route('/health', methods=['GET'])
+@app.route("/health", methods=["GET"])
 def health_status():
-    return '200 status ok'
+    return "200 status ok"
 
 
-@app.route('/team')
+@app.route("/team")
 def team_page():
-    return render_template('team.html', title="Team Dragon's Den")
+    return render_template("team.html", title="Team Dragon's Den")
 
 
-@app.route('/register', methods=('GET', 'POST'))
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         print(request)
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -116,21 +121,21 @@ def register():
 
     # TODO: Return a restister page
     # return "Register Page not yet implemented", 501
-    return render_template('register.html')
+    return render_template("register.html")
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
@@ -139,4 +144,4 @@ def login():
 
     # TODO: Return a login page
     # return "Login Page not yet implemented", 501
-    return render_template('login.html')
+    return render_template("login.html")
